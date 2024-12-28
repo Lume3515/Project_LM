a928c53c04a9c429b1e4a11c004877c22d7588ef
Loomip <taeun3515@gmail.com>
2024-12-28 18:02:20 +0900

    [New] 회전 추가 및 애니메이션 추가와 조금 적용

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TreeEditor;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    // 카메라 위치 // 0 : 왼쪽 1 : 오른쪽 2 : 보스
+    [SerializeField] Transform[] cameraPos;
+    [SerializeField] int cameraIndex;
+
+    // 플레이어 트랜스폼
+    private Transform playerTr;
+
+    // 마우스 X축
+    private float mouseX;
+
+
+
+    private void Start()
+    {
+        playerTr = GameObject.FindWithTag("Player").transform;
+
+        for (int i = 0; i < 10; i++)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            mouseX = 0;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            cameraIndex = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            cameraIndex = 1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CameraMove();
+        Rotation();
+    }
+
+    private void CameraMove()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraPos[cameraIndex].position, 0.25f);
+    }
+
+    private void Rotation()
+    {
+        Debug.Log(transform.eulerAngles.x);
+
+        float angle = transform.eulerAngles.x;
+
+        #region// 상하제한.....
+        // 하 제한
+        if (angle > 30 && angle < 100)
+        {
+            if (Input.GetAxis("Mouse Y") > 0)
+            {
+                mouseX = Input.GetAxisRaw("Mouse Y");
+            }
+            else if (Input.GetAxis("Mouse Y") < 0)
+            {
+                mouseX = 0;
+            }
+        }
+        // 상 제한
+        else if (angle > 290 && angle < 330)
+        {
+            if (Input.GetAxis("Mouse Y") < 0)
+            {
+                mouseX = Input.GetAxisRaw("Mouse Y");
+            }
+            else if (Input.GetAxis("Mouse Y") > 0)
+            {
+                mouseX = 0;
+            }
+        }
+        else
+        {
+            mouseX = Input.GetAxisRaw("Mouse Y");
+        }
+        #endregion
+
+        transform.Rotate(-mouseX * 5, 0, 0);
+
+
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // 마우스 민감도
+    private float mouseSpeed;
+
+    private Animator playerAnimator;
+
+    private void Awake()
+    {
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        UnityEngine.Cursor.visible = false;
+
+        playerRb = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+
+        moveSpeed = 3.8f;
+
+        mouseSpeed = 5f;
+    }
+
+    private void Update()
+    {
+        Movement();
+        Rotation();
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            playerAnimator.SetBool("isSitDown", true);
+            sitDown = !sitDown;
+
+            if (!sitDown) playerAnimator.SetBool("isSitDown", false);
+        }
+
+    }
+
+    #region// 플레이어 이동
     // 움직임 속도
     private float moveSpeed;
 
@@ -18,20 +58,9 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 moveDirection;
 
     // 마우스 키 값(축 기준)
-    private float mouseY;      
+    private float mouseY;
 
-    private void Start()
-    {
-        playerRb = GetComponent<Rigidbody>();
 
-        moveSpeed = 10f;
-    }
-
-    private void Update()
-    {
-        Movement();
-        Rotation();
-    }
 
     // 움직임
     private void Movement()
@@ -40,16 +69,41 @@ public class PlayerMovement : MonoBehaviour
         moveX = Input.GetAxis("Horizontal");
         moveZ = Input.GetAxis("Vertical");
 
+        playerAnimator.SetBool("isWalk", true);
+
         // playerRb.velocity.y를 따로 뺀 이유는 playerRb.velocity.y를 0에 할당하면 moveSpeed가 곱해져서 총 벡터의 값이 0이 돼서 > 안되는거 같다!!(안 움직임)
         moveDirection = transform.TransformDirection(moveX, 0, moveZ) * moveSpeed + new Vector3(0, playerRb.velocity.y, 0);
 
         playerRb.velocity = moveDirection;
+
+        if (playerRb.velocity.magnitude == 0)
+        {
+            playerAnimator.SetBool("isWalk", false);
+        }
+        else
+        {
+
+            playerAnimator.SetBool("isSitDown", false);
+        }
     }
 
     private void Rotation()
     {
         mouseY = Input.GetAxisRaw("Mouse X");
 
-        transform.Rotate(0, mouseY, 0);
+        transform.Rotate(0, mouseY * mouseSpeed, 0);
     }
+    #endregion
+
+    #region// 앉기 
+
+    // true가 앉은 거
+    private bool sitDown;
+
+    private IEnumerator SitDown()
+    {
+        yield return null;
+    }
+
+    #endregion
 }
