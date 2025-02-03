27a2126747be6ad155c0cd910242c2ef1fb734c3
LuminousMemory <taeun3515@gmail.com>
2025-02-03 10:21:40 +0900

    [New] 맵 변경 및 개발 중단

--- Assets/Editor/ObjectArrangement.cs
+++ Assets/Editor/ObjectArrangement.cs
@@ -14,7 +14,7 @@ public class ObjectArrangement : MonoBehaviour
     private static void InPut()
     {
         // csv파일 생성? 작성
-        using (StreamWriter streamWriter = new StreamWriter($"C:/Users/User/source/Map.csv"))
+        using (StreamWriter streamWriter = new StreamWriter($"C:/Users/이태언/source/Map.csv"))
         {
             // 맵의 크기 만큼 배열 생성
             GameObject[] obj = new GameObject[GameObject.FindGameObjectsWithTag("Map").Length];
@@ -40,6 +40,7 @@ public class ObjectArrangement : MonoBehaviour
             mainObj.Add(GameObject.FindWithTag("Directional Light"));
             mainObj.Add(GameObject.FindWithTag("SpawnPos"));
             mainObj.Add(GameObject.FindWithTag("PlayerHP(World Space Canvas)"));
+           
 
             // 값 넣기
             foreach (GameObject item in obj)
--- Assets/Scripts/Basic_InGame/Player/Follow.cs
+++ Assets/Scripts/Basic_InGame/Player/Follow.cs
@@ -20,7 +20,7 @@ public class Follow : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 direction = Vector3.Lerp(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(playerTr.position.x, 0, playerTr.position.z), Time.deltaTime * 15);
-        transform.position = direction + addPos;
+        Vector3 direction = Vector3.Lerp(transform.position,playerTr.position + addPos, Time.deltaTime * 15);
+        transform.position = direction;
     }
 }
